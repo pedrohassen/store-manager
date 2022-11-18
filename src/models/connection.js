b68cc5c6109fd544f2d6657a40fd2cf8734af8ef
Pedro <pedro_hasse@yahoo.com.br>
2022-11-18 13:58:51 -0300

    fix: 🐛 Correção do connection pra passar nos testes no github

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
 
+require('dotenv').config(); // não se esqueça de configurar suas variáveis de ambiente aqui na configuração
+
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
